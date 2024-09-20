3748baea2761651fed8ea2342c5839625b221fc4
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2024-09-20 22:06:06 +0530

    new

--- frontend/App.js
+++ frontend/App.js
@@ -5,7 +5,7 @@ import {View,Text} from 'react-native'
 const App = () => {
   return (
     <View className="bg-black">
-      <Text className="text-blue-700 text-2xl font-bold">zljvnfxlkbnfxlk</Text>
+      <Text className="text-blue-700 text-2xl font-bold">The Shool.AI</Text>
     </View>
   )
 }
