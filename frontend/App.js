8fbdbc5065d9cc59ebc2377aff8f03863d195e72
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2024-09-24 01:02:28 +0530

    Icons and drawer setup and configuration done

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,23 @@
 
-
 import React, { useState } from 'react';
 import {Text, View} from 'react-native';
 import TopNavigation from './src/navigation/TopNavigation';
 import BottomTabNavigation from './src/navigation/BottomTabNavigation';
+import DrawerNavigation from './src/navigation/DrawerNavigation';
 
 const App = () => {
    
   return(
     <>
     {/* <TopNavigation /> */}
-    <BottomTabNavigation />
+    {/* <BottomTabNavigation /> */}
+    <DrawerNavigation/>
     </>
   ) 
     
 };
 
 export default App;
+
+
 
--- frontend/src/navigation/DrawerNavigation.js
+++ frontend/src/navigation/DrawerNavigation.js
@@ -1,24 +1,80 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import HomeScreen from '../pages/HomeScreen';
-import TestEntryDetails from '../pages/TestEntryDetails';
-import WelcomeScreen from '../pages/WelcomeScreen';
+import React, {useRef, useState} from 'react';
+import {
+  Button,
+  DrawerLayoutAndroid,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+// import Icon from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState('left');
+  const changeDrawerPosition = () => {
+    if (drawerPosition === 'left') {
+      setDrawerPosition('right');
+    } else {
+      setDrawerPosition('left');
+    }
+  };
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close it"
+        onPress={() => drawer.current.closeDrawer()}
+      />
+    </View>
+  );
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="TestEntry" component={TestEntryDetails} />
-        <Drawer.Screen name="Study" component={WelcomeScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={drawerPosition}
+      renderNavigationView={navigationView}>
+      <View style={styles.container}>
+        {/* <Text style={styles.paragraph}>Drawer on the {drawerPosition}!</Text>
+        <Button
+          title="Change Drawer Position"
+          onPress={() => changeDrawerPosition()}
+        /> */}
+        {/* <Text style={styles.paragraph}>
+          Swipe from the side or press button below to see it!
+        </Text> */}
+        <Button
+          title="Open drawer"
+          onPress={() => drawer.current.openDrawer()}
+        />
+      {/* <Icon name="chart" size={30} color="#900" /> */}
+      <Icon name="home" size={30} color="#4F8EF7" />
+    
+     
+      </View>
+    </DrawerLayoutAndroid>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display:'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    padding:16
+
+  },
+  navigationContainer: {
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+});
 
 export default DrawerNavigation;
