c584e456fb019cc480bc13988a47df53eee82cfe
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2024-10-17 08:43:56 +0530

    changes

--- frontend/src/pages/WelcomeScreen.js
+++ frontend/src/pages/WelcomeScreen.js
@@ -41,6 +41,7 @@ const WelcomeScreen = ({navigation}) => {
         style={{
           direction: 'flex',
           flexDirection: 'row',
+          flex: 1,
           justifyContent: 'space-between',
           alignItems: 'center',
           margin: 10,
@@ -70,145 +71,126 @@ const WelcomeScreen = ({navigation}) => {
         </View>
         <Icon name="notifications-outline" size={30} />
       </View>
-      <Text
-        style={{
-          fontSize: 30,
-          marginHorizontal: 10,
-          color: 'black',
-          fontFamily: 'Poppins Medium',
-        }}>
-        Let's Learn Something New....
-      </Text>
-      <FlatList
-        data={data}
-        numColumns={2}
-        columnWrapperStyle={{gap: 10}}
-        contentContainerStyle={{gap: 10}}
-        keyExtractor={(Item, idx) => Item.name + idx}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignContent: 'center',
-                flexDirection: 'row',
-                backgroundColor: 'white',
-                flex: 1,
-                height: 150,
-                borderRadius: 20,
-                marginHorizontal: 10,
-                borderColor: 'black',
-              }}
-              onPress={() => navigation.navigate(item.route)}>
-              <Image
-                source={item.img}
-                style={{width: '100%', height: '100%', borderRadius: 20}}
-                resizeMode="contain"
-              />
-              <Text
+      <View style={{flex: 4}}>
+        <Text
+          style={{
+            fontSize: 25,
+            marginHorizontal: 10,
+            color: 'black',
+            fontFamily: 'Poppins Medium',
+          }}>
+          Let's Learn Something New....
+        </Text>
+
+        <FlatList
+          data={data}
+          numColumns={2}
+          columnWrapperStyle={{gap: 10}}
+          contentContainerStyle={{gap: 10}}
+          keyExtractor={(Item, idx) => Item.name + idx}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
                 style={{
-                  position: 'absolute',
-                  backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                  left: 0,
-                  right: 0,
-                  textAlign: 'left',
-                  color: 'black',
-                  fontFamily: 'Poppins Medium',
-                  fontSize: 22,
-                  paddingHorizontal: 10,
-                  paddingVertical: 5,
-                  borderRadius: 20,
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignContent: 'center',
+                  flexDirection: 'row',
+                  backgroundColor: 'white',
+                  flex: 1,
                   height: 150,
-                }}>
-                {item.name}
-              </Text>
-              <Text
-                style={{
-                  position: 'absolute',
-                  left: 0,
-                  right: 0,
-
-                  textAlign: 'left',
-                  color: 'black',
-                  fontFamily: 'Poppins Medium',
-                  width: 150,
-                  paddingHorizontal: 10,
-                  paddingTop: 70,
                   borderRadius: 20,
-                  height: 200,
-                }}>
-                100+ practice Questions
-              </Text>
-            </TouchableOpacity>
-          );
-        }}
-      />
-      <Text
-        style={{
-          fontSize: 19,
-          marginHorizontal: 10,
-          color: 'black',
-          fontWeight: '200',
-          fontFamily: 'Poppins Medium',
-        }}>
-        Words of Leaders: Fuel for Your Motivation
-      </Text>
-      <FlatList
-        data={dataContinue}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity
+                  marginHorizontal: 5,
+                  borderColor: 'black',
+                }}
+                onPress={() => navigation.navigate(item.route)}>
+                {/* <Surface elevation={4}> */}
+                  <Image
+                    source={item.img}
+                    style={{width: '100%', height: '100%', borderRadius: 20}}
+                    resizeMode="contain"
+                  />
+                  <Text
+                    style={{
+                      position: 'absolute',
+                      backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                      left: 0,
+                      right: 0,
+                      textAlign: 'left',
+                      color: 'black',
+                      fontFamily: 'Poppins Medium',
+                      fontSize: 20,
+                      paddingHorizontal: 10,
+                      paddingVertical: 5,
+                      borderRadius: 20,
+                      height: 150,
+                    }}>
+                    {item.name}
+                  </Text>
+                  <Text
+                    style={{
+                      position: 'absolute',
+                      left: 0,
+                      right: 0,
+                      top: 100,
+                      textAlign: 'left',
+                      color: 'black',
+                      fontFamily: 'Poppins Medium',
+                      width: 150,
+                      paddingHorizontal: 10,
+                      borderRadius: 20,
+                    }}>
+                    100+ practice Questions
+                  </Text>
+                {/* </Surface> */}
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+      <View style={{flex: 10}}>
+        <Text
+          style={{
+            fontSize: 19,
+            marginHorizontal: 10,
+            color: 'black',
+            fontWeight: '200',
+            fontFamily: 'Poppins Medium',
+          }}>
+          Words of Leaders: Fuel for Your Motivation
+        </Text>
+        {dataContinue.map((item, index) => (
+          <Surface
+            elevation={2}
+            style={{
+              margin: 10,
+              padding: 10,
+              borderRadius: 10,
+              marginVertical: 15,
+            }}
+            key={index}>
+            <Text
               style={{
-                margin: 10,
-                // backgroundColor: '#f1f1f1',
-                // borderWidth: 1,
-                // paddingVertical: 20,
-                // borderRadius: 10,
-                // shadowColor: "black",
-                // shadowOffset: {width: 0, height: 10},
-                // shadowOpacity: 0.25,
-                // shadowRadius: 1,
-                // elevation: 2,
+                textAlign: 'center',
+                color: 'black',
+                fontSize: 12,
+                fontFamily: 'Poppins Italic',
               }}>
-              <Surface
-                elevation={4}
-                style={{
-                  margin: 10,
-                  // backgroundColor: '#f1f1f1',
-                  // borderWidth: 1,
-                  padding: 10,
-                  borderRadius: 10,
-                  // shadowColor: "black",
-                  // shadowOffset: {width: 0, height: 10},
-                  // shadowOpacity: 0.25,
-                  // shadowRadius: 1,
-                  // elevation: 2,
-                }}>
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    color: 'black',
-                    fontSize: 12,
-                    fontFamily: 'Poppins Italic',
-                  }}>
-                  {item.content}
-                </Text>
+              {item.content}
+            </Text>
 
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    color: 'black',
-                    fontFamily: 'Poppins Italic',
-                  }}>
-                  {item.name}
-                </Text>
-              </Surface>
-            </TouchableOpacity>
-          );
-        }}
-      />
+            <Text
+              style={{
+                textAlign: 'center',
+                color: 'black',
+                fontFamily: 'Poppins Italic',
+              }}>
+              {item.name}
+            </Text>
+          </Surface>
+        ))}
+      </View>
     </View>
   );
 };
